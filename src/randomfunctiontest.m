2a329f32e38810acebdbc901afa017737053bafd
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-03-03 10:39:14 +0330

    other test cases added

--- src/randomfunctiontest.m
+++ src/randomfunctiontest.m
@@ -1,23 +1,40 @@
+% test randomfunction
+nRows = 4;
+nColumns = 6;
 
+% preconditions
+testMat = rand([nRows, nColumns]);
 
 %% Case 1 : Any option, rectangular matrix more rows than columns
-testMat = random([3, 2]);
-option = random(1);
-output = randomfunction(testMat, option);
-assert(output == testMat + [1, 2, 3].', "Case 1 failed")
+option = rand(1);
+[outputProcessed, ~] = randomfunction(testMat, option);
+assert(outputProcessed == testMat + [1:nRows].', "Case 1 failed")
 
 
 %% Case 2 : Any option, rectangular matrix, more columns than rows
-testMat = random([2, 3]);
-option = random(1);
-output = randomfunction(testMat, option);
-assert(output == testMat + [1, 2, 3], "Case 2 failed")
+option = rand(1);
+[outputProcessed, ~] = randomfunction(testMat, option);
+assert(outputProcessed == testMat + [1:nColumns], "Case 2 failed")
 
 
 %% Case 3 : Any option, square matrix
-testMat = random([3, 3]);
-option = random(1);
-output = randomfunction(testMat, option);
-assert(output == flip(flip(testMat, 2).', 2), "Case 2 failed")
+testMat = rand([3, 3]);
+option = rand(1);
+[outputProcessed, ~] = randomfunction(testMat, option);
+assert(outputProcessed == flip(flip(testMat, 2).', 2), "Case 3 failed")
 
 %% Case 4 : Option = 1, 
+option = 1;
+[outputProcessed, ~] = randomfunction(testMat, option);
+indices = logical(eye([nRows, nColumns]));
+testMat(indices) = sort(diag(testMat));
+assert(outputProcessed== testMat, "Case 4 failed")
+
+
+%% Case 5 : Option = 2
+option = 2;
+[outputProcessed, outputRaw] = randomfunction(testMat, option);
+indices = logical(eye([nRows, nColumns]));
+testMat(indices) = sort(diag(testMat), 'descend');
+assert(output == testMat, "Case 5 failed")
+
