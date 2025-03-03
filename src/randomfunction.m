5385ecc91a9c2674203d8bcc62e256f30d614eb8
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-03-03 11:17:41 +0330

    function written, all test pass

--- src/randomfunction.m
+++ src/randomfunction.m
@@ -1,8 +1,36 @@
-function [ output ] = randomfunction(inputMat, option)
+function [ outputProcessed, outputRaw ] = randomfunction(inputMat, option)
   arguments
     inputMat (:, :) double
     option (1, 1) double {mustBeNumeric}
   end % arguments
 
+[nRows, nColumns] = size(inputMat);
+outputRaw = inputMat;
+switch option
+    case 1
+        % put an algorithm here
+        indices = logical(eye([nRows, nColumns]));
+        outputProcessed = inputMat;
+        outputProcessed(indices) = sort(diag(inputMat));
+    
+    case 2
+        % put an algorithm here
+        indices = logical(eye([nRows, nColumns]));
+        outputProcessed = inputMat;
+        outputProcessed(indices) = sort(diag(inputMat), 'descend');
+    
+    otherwise
+        if nRows > nColumns
+            % put an algorithm here
+            outputProcessed = inputMat + transpose(1:nRows);
+
+        elseif nColumns > nRows
+            % put an algorithm here
+            outputProcessed = inputMat + [1:nColumns];
+        else
+            % put an algorithm here
+            outputProcessed = flip(flip(inputMat, 2).', 2);
+        end
+end
 
 end % function
--- src/randomfunctiontest.m
+++ src/randomfunctiontest.m
@@ -1,40 +1,45 @@
-% test randomfunction
-nRows = 4;
-nColumns = 6;
-
-% preconditions
-testMat = rand([nRows, nColumns]);
-
 %% Case 1 : Any option, rectangular matrix more rows than columns
+nRows = 6;
+nColumns = 4;
 option = rand(1);
+testMat = rand([nRows, nColumns]);
 [outputProcessed, ~] = randomfunction(testMat, option);
-assert(outputProcessed == testMat + [1:nRows].', "Case 1 failed")
+assert(isequal(outputProcessed, testMat + transpose(1:nRows)))
 
 
 %% Case 2 : Any option, rectangular matrix, more columns than rows
 option = rand(1);
+nRows = 4;
+nColumns = 6;
+testMat = rand([nRows, nColumns]);
 [outputProcessed, ~] = randomfunction(testMat, option);
-assert(outputProcessed == testMat + [1:nColumns], "Case 2 failed")
+assert(isequal(outputProcessed, testMat + [1:nColumns]))
 
 
 %% Case 3 : Any option, square matrix
 testMat = rand([3, 3]);
 option = rand(1);
 [outputProcessed, ~] = randomfunction(testMat, option);
-assert(outputProcessed == flip(flip(testMat, 2).', 2), "Case 3 failed")
+assert(isequal(outputProcessed, flip(flip(testMat, 2).', 2)))
 
 %% Case 4 : Option = 1, 
 option = 1;
+nRows = 6;
+nColumns = 4;
+testMat = rand([nRows, nColumns]);
 [outputProcessed, ~] = randomfunction(testMat, option);
 indices = logical(eye([nRows, nColumns]));
 testMat(indices) = sort(diag(testMat));
-assert(outputProcessed== testMat, "Case 4 failed")
+assert(isequal(outputProcessed, testMat))
 
 
 %% Case 5 : Option = 2
 option = 2;
-[outputProcessed, outputRaw] = randomfunction(testMat, option);
+nRows = 6;
+nColumns = 4;
+testMat = rand([nRows, nColumns]);
+[outputProcessed, ~] = randomfunction(testMat, option);
 indices = logical(eye([nRows, nColumns]));
 testMat(indices) = sort(diag(testMat), 'descend');
-assert(output == testMat, "Case 5 failed")
+assert(isequal(outputProcessed, testMat))
 
