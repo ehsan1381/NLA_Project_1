99588e4a3ef99ddebdca74c6038d39151c335886
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-03-06 16:21:57 +0330

    Testing modified cases of no option with square matrices now also have algorithms using for loops which are different than the function itself. 4 more test cases are added for option =1, 2 for square matrices and having more columns

--- src/randomfunctiontest.m
+++ src/randomfunctiontest.m
@@ -32,10 +32,20 @@ assert(isequal(outputProcessed, testMat));
 
 
 %% Case 3 : Random option, square matrix
-testMat = rand([3, 3]);
+nRows = 3;
+nColumns = 3;
+testMat = rand([nRows, nColumns]);
 option = rand(1);
 [outputProcessed, ~] = randomfunction(testMat, option);
-assert(isequal(outputProcessed, flip(flip(testMat, 2).', 2)))
+for iRow = 1:nRows - 1
+  for iColumn = 1:nColumns - 1
+    temp = testMat(iRow, iColumn);
+    testMat(iRow, iColumn) = testMat(nRows - iColumn + 1, nRows - iRow + 1);
+    testMat(nRows - iColumn + 1, nRows - iRow + 1) = temp;
+  end % for iColumn
+end % for iRow
+assert(isequal(outputProcessed, testMat));
+
 
 %% Case 4 : Option = 1,
 option = 1;
@@ -104,6 +114,84 @@ assert(isequal(outputProcessed, testMat));
 
 
 %% Case 8 : No option, square matrix
-testMat = rand([3, 3]);
+nRows = 3;
+nColumns = 3;
+testMat = rand([nRows, nColumns]);
 [outputProcessed, ~] = randomfunction(testMat);
-assert(isequal(outputProcessed, flip(flip(testMat, 2).', 2)))
+
+for iRow = 1:nRows - 1
+  for iColumn = 1:nColumns - 1
+    temp = testMat(iRow, iColumn);
+    testMat(iRow, iColumn) = testMat(nRows - iColumn + 1, nRows - iRow + 1);
+    testMat(nRows - iColumn + 1, nRows - iRow + 1) = temp;
+  end % for iColumn
+end % for iRow
+assert(isequal(outputProcessed, testMat));
+
+
+%% Case 9 : Option = 1, More colummns
+option = 1;
+nRows = 4;
+nColumns = 6;
+testMat = rand([nRows, nColumns]);
+[outputProcessed, ~] = randomfunction(testMat, option);
+
+nDiagonal = min([nRows, nColumns]);
+diagonal = zeros([nDiagonal, 1]);
+
+for i=1:nDiagonal
+  diagonal(i) = testMat(i, i);
+end % for
+
+diagonal = sort(diagonal);
+assert(isequal(diag(outputProcessed), diagonal));
+
+
+%% Case 10 : Option = 2, More columns
+option = 2;
+nRows = 4;
+nColumns = 6;
+testMat = rand([nRows, nColumns]);
+nDiagonal = min([nRows, nColumns]);
+diagonal = zeros([nDiagonal, 1]);
+
+for i=1:nDiagonal
+  diagonal(i) = testMat(i, i);
+end % for
+[outputProcessed, ~] = randomfunction(testMat, option);
+diagonal = sort(diagonal, 'descend');
+assert(isequal(diag(outputProcessed), diagonal));
+
+%% Case 11 : Option = 1, square
+option = 1;
+nRows = 6;
+nColumns = 6;
+testMat = rand([nRows, nColumns]);
+[outputProcessed, ~] = randomfunction(testMat, option);
+
+nDiagonal = min([nRows, nColumns]);
+diagonal = zeros([nDiagonal, 1]);
+
+for i=1:nDiagonal
+  diagonal(i) = testMat(i, i);
+end % for
+
+diagonal = sort(diagonal);
+assert(isequal(diag(outputProcessed), diagonal));
+
+%% Case 12 : Option = 2, square
+option = 2;
+nRows = 6;
+nColumns = 6;
+testMat = rand([nRows, nColumns]);
+nDiagonal = min([nRows, nColumns]);
+diagonal = zeros([nDiagonal, 1]);
+
+for i=1:nDiagonal
+  diagonal(i) = testMat(i, i);
+end % for
+[outputProcessed, ~] = randomfunction(testMat, option);
+diagonal = sort(diagonal, 'descend');
+assert(isequal(diag(outputProcessed), diagonal));
+
+
